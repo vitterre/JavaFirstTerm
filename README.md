e2dfb05a30bb1145c1386cf3d7161828d2e6479e
vitterre <bagatovbb@gmail.com>
2022-09-07 10:16:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# JavaFirstTerm
+
+Homework and practice on "Computer Science and programming"
+
+Student: Bagat Bagatov
+
+Group: 11-205
+
---
+++ build.sh
@@ -0,0 +1,14 @@
+BASEDIR=$(dirname "$0")
+CLASSDIR="$BASEDIR/out/production/JavaFirstTerm"
+if [ -d "$CLASSDIR" ]; then
+  javac $1 -d $CLASSDIR
+else
+  mkdir "$BASEDIR/out"
+  mkdir "$BASEDIR/out/production"
+  mkdir "$BASEDIR/out/production/JavaFirstTerm"
+  javac $1 -d $CLASSDIR
+fi
+
+FULLRUNNAME=$(basename "$1")
+RUNNAME="${FULLRUNNAME%.*}"
+java -cp $CLASSDIR $RUNNAME
