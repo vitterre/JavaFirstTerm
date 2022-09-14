81523d9e6a4cfa161e8d5ba0e6ba81a99b96f76e
vitterre <bagatovbb@gmail.com>
2022-09-14 17:55:19 +0300

    HW 09.09.2022

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,19 @@
-class Operators {
-	public static final int OPERATOR_AND = 0;
-	public static final int OPERATOR_OR  = 1;
-	public static final int OPERATOR_XOR = 2;
-	public static final int OPERATOR_NOT = 3;
-}
-
 public class Main {
-	private static final char[] operatorsChar = {
-					'&', '|', '^', '!'
-	};
+	private static double exp(double x) {
+		double result = 1.0d;
+		double current = x;
+		int i = 1;
 
-	private static boolean resultOfBooleanFunction(int operator, boolean a, boolean b) {
-		switch (operator) {
-			case 0 -> {
-				return a & b;
-			}
-			case 1 -> {
-				return a | b;
-			}
-			case 2 -> {
-				return a ^ b;
-			}
-			default -> {
-				return false;
-			}
+		while (current >= 0.00001d) {
+			result += current;
+			current *= x / (i + 1);
+			++i;
 		}
-	}
 
-	private static void printBooleanTable(int operator) {
-		if (operator != 3) {
-			System.out.println("+-----+-------+-------+-------+-------+");
-			System.out.println("|     |   " +
-							operatorsChar[operator] + "   |   " +
-							operatorsChar[operator] + "   |   " +
-							operatorsChar[operator] + "   |   " +
-							operatorsChar[operator] + "   |");
-			System.out.println("+-----+-------+-------+-------+-------+");
-			System.out.println("|  a  | true  | false | true  | false |");
-			System.out.println("|-----|-------|-------|-------|-------|");
-			System.out.println("|  b  | true  | true  | false | false |");
-			System.out.println("|-----|-------|-------|-------|-------|");
-			System.out.println("| res | " +
-							resultOfBooleanFunction(operator, true, true) + "  | " +
-							resultOfBooleanFunction(operator, false, true) + " | " +
-							resultOfBooleanFunction(operator, true, false) + " | " +
-							resultOfBooleanFunction(operator, false, false) + " |");
-			System.out.println("+-----+-------+-------+-------+-------+");
-		} else {
-			System.out.println("+-----+-------+-------+");
-			System.out.println("|     |   !   |   !   |");
-			System.out.println("+-----+-------+-------+");
-			System.out.println("|  a  | true  | false |");
-			System.out.println("|-----|-------|-------|");
-			System.out.println("| res | " + !true + " | " + !false + "  |");
-			System.out.println("+-----+-------+-------+");
-		}
+		return result;
 	}
 
 	public static void main(String[] args) {
-		printBooleanTable(Operators.OPERATOR_AND);
-		printBooleanTable(Operators.OPERATOR_OR);
-		printBooleanTable(Operators.OPERATOR_XOR);
-		printBooleanTable(Operators.OPERATOR_NOT);
+		System.out.println(exp(20));
 	}
 }
