c83559364f9f9bb2b15b559711ca2139a6029612
vitterre <bagatovbb@gmail.com>
2022-09-29 16:55:55 +0300

    PR 29.09.2022

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,32 @@
-import com.vitterre.tasks.CircleBuilder;
-import com.vitterre.tasks.SimpleMathTasks;
-import com.vitterre.tasks.TreeForce;
-
-import java.util.Arrays;
-
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("# 01");
-		System.out.println("INPUT: 7");
-		TreeForce.build(7);
-		System.out.println("=========================");
+		// HW: Draw golden apple :D
+		int[][][] image = {
+						{       {255,   0, 100, 255},
+										{200, 200, 200, 200},
+										{  0,   0,   0, 100},
+										{200, 100,  40,  30}, /* LINES */}, // RED
+						{       {255,  20, 100, 200},
+										{100,   0,   0, 200},
+										{ 10, 100,   0, 200},
+										{120, 255, 255,   0}  /* LINES */}, // GREEN
+						{       {255,   0, 200, 200},
+										{120, 240,   0,   0},
+										{255,   0, 100,   0},
+										{100, 120,   0,   0}  /* LINES */}  // BLUE
+		};
 
-		System.out.println("# 02");
-		System.out.println("INPUT: 1 10");
-		SimpleMathTasks.getNumbersBetween(1, 10);
-		System.out.println("=========================");
+		final String RESET = "\u001B[0m";
+		final String RGB = "\u001B[38;2;255;200;255m";
+		final String R = "\u2588";
 
-		System.out.println("# 03");
-		System.out.println("INPUT 20");
-		System.out.println(SimpleMathTasks.sqrt(20));
-		System.out.println("=========================");
-
-		System.out.println("# 04");
-		System.out.println("INPUT 20");
-		CircleBuilder.build(20);
-		System.out.println("=========================");
-
-		System.out.println("# 05");
-		System.out.println("INPUT 0.5");
-		System.out.println(SimpleMathTasks.sin(0.5));
-		System.out.println("=========================");
-
-		System.out.println("# 06");
-		System.out.println("INPUT 1 -7 6");
-		System.out.println(Arrays.toString(SimpleMathTasks.solveSquareEquation(1, -7, 6)));
-		System.out.println("=========================");
-
-		System.out.println("# 07");
-		System.out.println("INPUT 5 321 654 78119 9186 987");
-		System.out.println(SimpleMathTasks.findNumbersWhereDigitsAreDescending(5));
-		System.out.println("=========================");
-
-		System.out.println("# 08");
-		System.out.println("INPUT 20");
-		System.out.println(Arrays.toString(SimpleMathTasks.getArrayOf(20)));
-		System.out.println("=========================");
-
-		System.out.println("# 09");
-		System.out.println("INPUT 5 7 8 1 2 9");
-		System.out.println(SimpleMathTasks.getNumber(args));
-		System.out.println(SimpleMathTasks.getReversedNumber(args));
-		System.out.println("=========================");
-
-		final String[] strings = {"abafasba", "babblajlbj", "lkbalabbbjhal", "alkjba", "lkbjalabb"};
-		final String subString = "abb";
-		System.out.println("# 10");
-		System.out.println("INPUT " + Arrays.toString(strings) + " " + subString);
-		System.out.println(SimpleMathTasks.getIndexesOfElementsThatContainsSubString(strings, subString));
-		System.out.println("=========================");
+		for (int i = 0; i < 4; ++i) {
+			for (int j = 0; j < 4; ++j) {
+				System.out.printf("\u001B[38;2;%d;%d;%dm", image[0][i][j], image[1][i][j], image[2][i][j]);
+				System.out.print(R);
+				System.out.print(RESET);
+			}
+			System.out.println();
+		}
 	}
 }
